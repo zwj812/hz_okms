9798a2eb596d95ebe1ee798a46aa2826965f6847
zwj812 <zhaowenjiwork@126.com>
2020-09-24 14:19:50 +0800

    test 0.0.3

--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -12,5 +12,6 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         Log.e("test","0.0.2");
+        Log.e("test","0.0.3");
     }
 }
