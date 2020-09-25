c36104b920e610abd921576c9f756af7508b8c33
zwj812 <zhaowenjiwork@126.com>
2020-09-25 16:34:10 +0800

    test分支2

--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -13,7 +13,6 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         Log.e("test","0.0.2");
         Log.e("test","0.0.3");
-        Log.e("test分支","111111");
         Log.e("test分支2","22222");
 
     }
